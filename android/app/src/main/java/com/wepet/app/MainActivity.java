8c49796aedfb707da0328cb30320a1c09989b80c
xzy666-xzy <zhenyangx884@gmail.com>
2026-05-01 20:31:38 +0900

    feat: update pages and add api client

--- android/app/src/main/java/com/wepet/app/MainActivity.java
+++ android/app/src/main/java/com/wepet/app/MainActivity.java
@@ -1,5 +1,18 @@
 package com.wepet.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setBackgroundDrawableResource(com.wepet.app.R.color.splash_background);
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        webView.setBackgroundColor(Color.parseColor("#FF6A00"));
+    }
+}
